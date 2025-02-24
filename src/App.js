365fd133c2b991be8b4a35016860d52a14b09e10
MilenTecle <m_tecle@hotmail.com>
2025-02-24 11:33:13 +0100

    import useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import './App.module.css';
